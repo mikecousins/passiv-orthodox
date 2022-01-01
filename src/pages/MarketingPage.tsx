69b7d62607b54a2056c8217b4bf268be3bcb3797
Mike Cousins <mike@mikecousins.com>
2022-01-01 14:41:32 -0700

    Updating marketing page

--- src/pages/MarketingPage.tsx
+++ src/pages/MarketingPage.tsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 const MarketingPage = () => (
   <div>
-    <div className="text-2xl font-serif">
+    <div className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
       Passiv Orthodox
     </div>
-    <div>
+    <div className="text-xl mt-2">
       Everything you need, nothing you don't
     </div>
     <Link to="/dashboard">Login</Link>

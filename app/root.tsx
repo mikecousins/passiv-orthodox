29e7488d57581996835237aef40a7c61c99e6a2b
Mike Cousins <mike@mikecousins.com>
2022-06-11 14:57:34 -0600

    Styling the main app

--- app/root.tsx
+++ app/root.tsx
@@ -7,9 +7,9 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import { QueryClient, QueryClientProvider } from 'react-query';
-import axios from 'axios';
-import AuthProvider from './providers/AuthProvider';
+import { QueryClient, QueryClientProvider } from "react-query";
+import axios from "axios";
+import AuthProvider from "./providers/AuthProvider";
 import styles from "./styles/app.css";
 
 export const meta: MetaFunction = () => ({
@@ -24,7 +24,7 @@ export function links() {
 
 export default function App() {
   const queryClient = new QueryClient();
-  axios.defaults.baseURL = 'https://api.passiv.com/api/v1/';
+  axios.defaults.baseURL = "https://api.passiv.com/api/v1/";
   return (
     <html lang="en">
       <head>
@@ -32,11 +32,17 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <QueryClientProvider client={queryClient}>
-          <AuthProvider>
-            <Outlet />
-          </AuthProvider>
-        </QueryClientProvider>
+        <div className="bg-gray-900 h-screen text-gray-300 flex flex-col">
+          <div className="flex-1" />
+          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <QueryClientProvider client={queryClient}>
+              <AuthProvider>
+                <Outlet />
+              </AuthProvider>
+            </QueryClientProvider>
+          </div>
+          <div className="flex-1" />
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />

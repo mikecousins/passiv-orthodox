a807a0375faa20e87d87bc55745a9f5dc474d45a
Mike Cousins <mike@mikecousins.com>
2022-06-11 15:02:08 -0600

    Fixing page title

--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,7 @@ import styles from "./styles/app.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Passiv Orthodox",
   viewport: "width=device-width,initial-scale=1",
 });
 
